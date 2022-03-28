6bead02fbcc89095c6d35c7f1776152b74829cc0
Ian Zaque Santos <43974640+ian-zaque@users.noreply.github.com>
2022-03-27 22:26:40 -0300

    add dinamico de lixeiras

--- app/src/App.py
+++ app/src/App.py
@@ -1,81 +1,126 @@
+from os import remove
 import tkinter as tk
 import tkinter.font as tkFont
 # from Lixeira import Lixeira 
 
 class App:
-    def __init__(self, root):        
+    def __init__(self, window):
+        self.window = window
+        self.lixeiras = []
+        
+        # Variables to control itens placement
+        self.widthButton = 55; self.heightButton = 25;
+        self.xImg = 70; self.yImg = 40; self.widthImg = 106; self.heightImg = 84;
+        self.xAddButton = 10; self.yAddButton = 40;
+        self.xRemoveButton = 10; self.yRemoveButton = 70;
+        self.xBlockButton = 10; self.yBlockButton = 100;
+        self.xPercentage = 90; self.yPercentage = 10; self.widthPercentage=70; self.heightPercentage=25
+        
         #setting title
-        root.title("Coleta de Lixo")
-        #setting window size
-        width=600
-        height=500
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
+        self.window.title("Coleta de Lixo")
+        #setting self.window size
+        width=600; height=500
+        screenwidth = self.window.winfo_screenwidth()
+        screenheight = self.window.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
+        self.window.geometry(alignstr)
+        self.window.resizable(width=False, height=False)
 
-        btnCreateLixeiras = tk.Button(root)
-        btnCreateLixeiras=tk.Button(root)
+        ipLixeiraButton=tk.Button(self.window)
+        ipLixeiraButton["bg"] = "#efefef"
+        ipLixeiraButton["font"] = tkFont.Font(family='Times',size=10)
+        ipLixeiraButton["fg"] = "#000000"
+        ipLixeiraButton["justify"] = "center"
+        ipLixeiraButton["text"] = "Remover"
+        ipLixeiraButton.place(x=10,y=70,width=55,height=25)
+        ipLixeiraButton["command"] = self.ipLixeiraButton_command
+
+        # Button that creates Lixeiras
+        btnCreateLixeiras = tk.Button(self.window)
+        btnCreateLixeiras=tk.Button(self.window)
         btnCreateLixeiras["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        btnCreateLixeiras["font"] = ft
+        btnCreateLixeiras["font"] = tkFont.Font(family='Times',size=10)
         btnCreateLixeiras["fg"] = "#000000"
         btnCreateLixeiras["justify"] = "center"
         btnCreateLixeiras["text"] = "Add Lixeira"
         btnCreateLixeiras.place(x=10,y=460,width=75,height=25)
-        btnCreateLixeiras["command"] = self.btnCreateLixeiras_command(root)
+        btnCreateLixeiras["command"] = self.btnCreateLixeiras_command
+    
+    
+    def createLixeira(self):
+        if len(self.lixeiras) == 0:
+            self.widthButton = 55; self.heightButton = 25;
+            self.xImg = 70; self.yImg = 40; self.widthImg = 106; self.heightImg = 84;
+            self.xAddButton = 10; self.yAddButton = 40;
+            self.xRemoveButton = 10; self.yRemoveButton = 70;
+            self.xBlockButton = 10; self.yBlockButton = 100;
+            self.xPercentage = 90; self.yPercentage = 10; self.widthPercentage=70; self.heightPercentage=25
         
-    
-    def createLixeira(self,root):
-        GLineEdit_514=tk.Entry(root)
-        GLineEdit_514["borderwidth"] = "1px"
+        else:
+            print('SIZE SIZE',self.lixeiras)
+            self.xImg = self.xImg+ 190;
+            self.xAddButton = self.xAddButton + 190;
+            self.xRemoveButton = self.xRemoveButton + 190;
+            self.xBlockButton = self.xBlockButton + 190;
+            self.xPercentage = self.xPercentage + 190;
+         
+        # img
+        img=tk.Entry(self.window)
+        img["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_514["font"] = ft
-        GLineEdit_514["fg"] = "#333333"
-        GLineEdit_514["justify"] = "center"
-        GLineEdit_514["text"] = "Lixeira N"
-        GLineEdit_514.place(x=70,y=40,width=106,height=84)
+        img["font"] = ft
+        img["fg"] = "#333333"
+        img["justify"] = "center"
+        # img["text"] = "Lixeira " + IP + ':' + str(PORT)
+        img["text"] = "Lixeira "
+        img.place(x = self.xImg, y = self.yImg, width = self.widthImg, height = self.heightImg)
 
-        GButton_655=tk.Button(root)
-        GButton_655["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_655["font"] = ft
-        GButton_655["fg"] = "#000000"
-        GButton_655["justify"] = "center"
-        GButton_655["text"] = "Add"
-        GButton_655.place(x=10,y=40,width=55,height=25)
-        GButton_655["command"] = self.GButton_655_command
+        # Add Lixo Button
+        addButton=tk.Button(self.window)
+        addButton["bg"] = "#efefef"
+        addButton["font"] = tkFont.Font(family='Times',size=10)
+        addButton["fg"] = "#000000"
+        addButton["justify"] = "center"
+        addButton["text"] = "Add"
+        addButton.place(x = self.xAddButton, y = self.yAddButton, width = self.widthButton, height =self.heightButton)
+        addButton["command"] = self.GButton_655_command
 
-        GButton_311=tk.Button(root)
-        GButton_311["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_311["font"] = ft
-        GButton_311["fg"] = "#000000"
-        GButton_311["justify"] = "center"
-        GButton_311["text"] = "Remover"
-        GButton_311.place(x=10,y=70,width=55,height=25)
-        GButton_311["command"] = self.GButton_311_command
+        # Remove Lixo BUtton
+        removeButton=tk.Button(self.window)
+        removeButton["bg"] = "#efefef"
+        removeButton["font"] = tkFont.Font(family='Times',size=10)
+        removeButton["fg"] = "#000000"
+        removeButton["justify"] = "center"
+        removeButton["text"] = "Remover"
+        removeButton.place(x=self.xRemoveButton, y=self.yRemoveButton, width=self.widthButton, height=self.heightButton)
+        removeButton["command"] = self.GButton_311_command
 
-        GButton_519=tk.Button(root)
-        GButton_519["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_519["font"] = ft
-        GButton_519["fg"] = "#000000"
-        GButton_519["justify"] = "center"
-        GButton_519["text"] = "Bloquear"
-        GButton_519.place(x=10,y=100,width=55,height=25)
-        GButton_519["command"] = self.GButton_519_command
+        # Bloquear Lixeira Button
+        blockButton=tk.Button(self.window)
+        blockButton["bg"] = "#efefef"
+        blockButton["font"] = tkFont.Font(family='Times',size=10)
+        blockButton["fg"] = "#000000"
+        blockButton["justify"] = "center"
+        blockButton["text"] = "Bloquear"
+        blockButton.place(x=self.xBlockButton, y=self.yBlockButton, width=self.widthButton, height=self.heightButton)
+        blockButton["command"] = self.GButton_519_command
 
-        GLabel_551=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_551["font"] = ft
-        GLabel_551["fg"] = "#333333"
-        GLabel_551["justify"] = "center"
-        GLabel_551["text"] = "%"
-        GLabel_551["relief"] = "flat"
-        GLabel_551.place(x=90,y=10,width=70,height=25)
-    
+        # Percentage Label
+        percentage=tk.Label(self.window)
+        percentage["font"] = tkFont.Font(family='Times',size=10)
+        percentage["fg"] = "#333333"
+        percentage["justify"] = "center"
+        percentage["text"] = "%"
+        percentage["relief"] = "flat"
+        percentage.place(x=self.xPercentage, y=self.yPercentage, width=self.widthPercentage, height=self.heightPercentage)        
+        
+        self.lixeiras.append({
+            'img': img,
+            'addButton' : addButton,
+            'removeButton': removeButton,
+            'blockButton' : blockButton,
+            'percentage': percentage
+        })
 
     def GButton_655_command(self):
         print("command")
@@ -88,10 +133,11 @@ class App:
     def GButton_519_command(self):
         print("command")
 
-    def btnCreateLixeiras_command(self,root):
-        self.createLixeira(root)
+    def btnCreateLixeiras_command(self):
+        print('aaaaa',self.lixeiras)
+        self.createLixeira()
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+    window = tk.Tk()
+    app = App(window)
+    window.mainloop()
--- app/src/Client.py
+++ app/src/Client.py
@@ -4,9 +4,6 @@ import socket
 class Client:
     
     def __init__(self):
-        self.HOST = socket.gethostname()
-        print('CLIENT HOST: '+self.HOST)
-        # self.IP = socket.gethostbyname(self.HOST)
         self.IP = '127.0.0.1'
         self.PORT = 64064
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
